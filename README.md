e66e785e8b4217098bb3c164c27c7ab0e3930538
Doug Matthews <dougm121@gmail.com>
2020-05-01 11:29:03 -0700

    Initial work (#1)
    * Updated README.md and added requirements file.
    
    * basic image generation
    
    * basic image generation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # image-gen
 This project generates a labeled data set using a set of 3D models.
+
+# How to Run
+`blender --background base.blend --python generate_image.py`
+
+# Setup 
+## Install Requirements
+`pip install -r requirements.txt`
+
+## Create and Activate Virtual Environment (Optional)
+Install the virtual environment package
+
+`pip install virtualenv`
+
+Create a new virtual environment (You may choose your own name, but here we used "venv")
+
+`virtualenv venv`
+
+Activate the Virtual Environment
+
+`venv\Scripts\activate`
+
+Or on mac:
+`source venv/bin/activate`
---
+++ generate_image.py
@@ -0,0 +1,110 @@
+import bpy
+import os
+import glob
+import random
+import math
+import mathutils
+
+def random_point(center, inner_radius, outer_radius):
+    u = random.uniform(0, 1)
+    v = random.uniform(0, 1)
+    theta = u * 2.0 * math.pi
+    phi = math.acos(2.0 * v - 1.0)
+    r = random.uniform(inner_radius, outer_radius)
+    sinTheta = math.sin(theta)
+    cosTheta = math.cos(theta)
+    sinPhi = math.sin(phi)
+    cosPhi = math.cos(phi)
+    x = r * sinPhi * cosTheta
+    y = r * sinPhi * sinTheta
+    z = r * cosPhi
+    return  mathutils.Vector((x, y, z)) + center
+
+def look_at(obj_camera, point):
+    print(point)
+    loc_camera = obj_camera.location
+
+    print(loc_camera)
+    direction = point - loc_camera
+    # point the cameras '-Z' and use its 'Y' as up
+    rot_quat = direction.to_track_quat('-Z', 'Y')
+
+    # assume we're using euler rotation
+    obj_camera.rotation_euler = rot_quat.to_euler()
+
+
+def create_lights():
+    lights = []
+    center = mathutils.Vector((0, 0, 0))
+    for x in range(0, random.randint(1,4)):
+        bpy.ops.object.light_add(type='POINT', radius=1.0, location=random_point(center, 3, 15))
+        light = bpy.context.selected_objects[0]
+        light.data.energy = random.uniform(250.0,2000.0)
+        lights.append(bpy.context.selected_objects[0])
+    return lights
+
+# The direcetory that contains the models we want to include to label in our data.
+model_directory = "models"
+
+models = glob.glob(os.path.join(model_directory, "*.dae"))
+
+output_directory = "/Users/dougmatthews/Repos/image-gen/renders"
+
+# The directory that contains backgrounds to use in our data.
+background_directory = ""
+
+# Total number of images generated
+num_images = 30
+
+images_per_model = int(num_images / len(models))
+
+print (images_per_model)
+
+for model_path in models:
+    name = os.path.splitext(os.path.basename(model_path))[0]
+    bpy.ops.wm.collada_import(filepath=model_path, )
+
+    # make sure to get all imported objects
+    if len(bpy.context.selected_objects) > 1:
+        raise Exception("Error", "Image generation expects imported model to only have one object.")
+
+    imported_model = bpy.context.selected_objects[0]
+    imported_model.scale = (0.1, 0.1, 0.1)
+    print(imported_model)
+
+    mesh = imported_model.data
+    for f in mesh.polygons:
+        f.use_smooth = True
+
+    lights = []
+
+    # Render images
+    for image in range(0, images_per_model):
+        
+        
+        imported_model.data.materials[0].node_tree.nodes["Principled BSDF"].inputs[0].default_value = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1), 1)
+        camera = bpy.context.scene.camera
+        center = mathutils.Vector((0, 0, 0))
+
+        camera.location = random_point(center, 4, 20)
+
+        point = random_point(center, 0, 1)
+        look_at(camera, point)
+
+        bpy.context.scene.render.engine = 'CYCLES'
+        bpy.context.scene.render.image_settings.color_mode ='RGBA'
+        bpy.context.scene.render.image_settings.file_format='PNG' 
+        bpy.context.scene.render.image_settings.compression = 90
+
+        # Create lights
+        bpy.ops.object.select_all(action='DESELECT')
+        for light in lights: 
+            light.select_set(True)
+        bpy.ops.object.delete()
+        lights = create_lights()
+
+        bpy.context.scene.render.filepath = os.path.join(output_directory, f'{name}-{str(image)}.png')
+        bpy.ops.render.render(write_still=True)
+
+    imported_model.select_set(True)
+    bpy.ops.object.delete()
